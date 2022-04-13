f772592ed08d39ccec3145b33889be96874c872b
Josue Martinez <j.e.martinez1993@gmail.com>
2022-04-14 00:20:01 +0200

    constants added, status bar, displaying some data using component and assets

---
+++ components/FocusedStatusBar.js
@@ -0,0 +1,13 @@
+ import { StatusBar } from "expo-status-bar";
+ import { useIsFocused } from "@react-navigation/native";
+
+ const FocusedStatusBar = (props) => {
+     const isFocused = useIsFocused();
+      return ( 
+       isFocused ? 
+       <StatusBar animated={true} {...props}/> 
+       : null
+ );
+ }
+
+ export default FocusedStatusBar
---
+++ components/index.js
@@ -0,0 +1,7 @@
+import FocusedStatusBar from "./FocusedStatusBar.js";
+import HomeHeader from "./HomeHeader.js"
+import NFTCard from "./NFTCard.js"
+
+export {
+    FocusedStatusBar, HomeHeader, NFTCard
+};
