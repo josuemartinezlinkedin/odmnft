65ccd549ba9460b0a7e617927a8c950194cb65e2
Josue Martinez <j.e.martinez1993@gmail.com>
2022-04-15 13:19:54 +0200

    included import to odmnft logo in assets.js constants

--- constants/assets.js
+++ constants/assets.js
@@ -5,6 +5,7 @@ import left from "../assets/icons/left.png";
 import search from "../assets/icons/search.png";
 
 import logo from "../assets/images/logo.png";
+import odmnft1 from "../assets/images/odmnft1.png"
 import nft01 from "../assets/images/nft01.png";
 import nft02 from "../assets/images/nft02.jpeg";
 import nft03 from "../assets/images/nft03.jpeg";
@@ -26,6 +27,7 @@ export default {
   search,
 
   logo,
+  odmnft1,
   nft01,
   nft02,
   nft03,
