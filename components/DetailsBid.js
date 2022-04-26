b4856c6d4266b358382814074a4e604e8328e5e2
Josue Martinez <j.e.martinez1993@gmail.com>
2022-04-26 21:00:13 +0200

    DetailsBid component

--- components/DetailsBid.js
+++ components/DetailsBid.js
@@ -1,10 +1,36 @@
-import { View, Text } from 'react-native'
 import React from 'react'
+import { View, Text, Image } from 'react-native'
+import { COLORS, SIZES, FONTS } from '../constants' 
+import { EthPrice } from './SubInfo'
 
-const DetailsBid = () => {
+
+const DetailsBid = ({bid}) => {
   return (
-    <View style={{paddingTop: 5}}>
-      <Text>DetailsBid</Text>
+    <View>
+      <Image
+      source={bid.image}
+      resizeMode="contain"
+      style={{width:48, height:48}}
+      />
+      <View style={{paddingLeft: 12}}>
+        <Text style={{
+          fontFamily: FONTS.semiBold,
+          fontSize: SIZES.small, 
+          color: COLORS.primary
+        }}>
+          Bid placed by {bid.name}
+        </Text>
+        <Text style={{
+          fontFamily: FONTS.regular,
+          fontSize: SIZES.small -2 ,
+          color: COLORS.secondary,
+          marginTop: 3,
+          marginBottom:5
+        }}>
+          {bid.date}
+        </Text>
+      </View>
+      <EthPrice price={bid.price}/>
     </View>
   )
 }
--- screens/Details.js
+++ screens/Details.js
@@ -36,6 +36,16 @@ const Details = ({route, navigation}) => {
           <SubInfo/>
           <View style={{padding: SIZES.font}}>
             <DetailsDescr data={data}/>
+
+            {data.bids.length >0 && (
+              <Text style={{
+                fontSize:SIZES.font,
+                fontFamily: FONTS.semiBold,
+                color: COLORS.primary
+              }}>
+                Current Bids
+              </Text>
+            )}
           </View>
         </React.Fragment>
         
